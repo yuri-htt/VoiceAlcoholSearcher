f0a60406a74d9ac10152ba150190921bea2c0fc2
Yuri <yuri.tayama@linksports.co.jp>
2019-01-04 19:31:28 +0900

    🎨make view of Add screen

--- src/screens/add/index.js
+++ src/screens/add/index.js
@@ -1,13 +1,116 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { 
+  Text, 
+  View, 
+  TextInput, 
+  Dimensions, 
+  ScrollView,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+import StarRating from 'react-native-star-rating';
+
+import images from '../../components/images';
 import styles from './styles';
 
+const { width, height } = Dimensions.get('window');
+
 export default class Add extends Component {
+  constructor(props) {
+    super(props);
+
+    const { navigation } = this.props;
+    const item = navigation.getParam('item', null);
+
+    this.state = {
+      item,
+      titleText: '',
+      text: '',
+      starCount: 0,
+    };
+  }
+
   render() {
+    const { 
+      item,
+      starCount,
+      text,
+    } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Add</Text>
+        <View style={[{width}, styles.contents]}>
+          <View style={styles.sake}>
+            <Image
+                style={styles.icon}
+                source={this.getCategoryIcon(item.categoryName)}
+              />
+            <Text>{item.name}</Text>
+          </View>
+        
+          <View style={[styles.row, styles.starContainer]}>
+            <StarRating
+              disabled={false}
+              maxStars={5}
+              rating={starCount}
+              selectedStar={rating => this.onStarRatingPress(rating)}
+              starSize={28}
+              buttonStyle={{ marginHorizontal: 8 }}
+              fullStarColor="orange"
+              emptyStarColor="orange"
+            />
+          </View>
+
+          <View style={styles.textInputContainer}>
+            <TextInput
+              multiline
+              style={[styles.textInput]}
+              underlineColorAndroid="transparent"
+              placeholder="お酒の感想を記載しましょう"
+              textAlignVertical="top"
+              value={text}
+              onChangeText={this.onChangeText}
+            />
+          </View>
+
+          <View style={styles.btnContainer}>
+            <TouchableOpacity onPress={() => this.onPressSave()} style={styles.primaryBtn}>
+              <Text style={styles.primaryBtnTxt}>保存する</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => this.onPressSave()} style={styles.seondaryBtn}>
+              <Text style={styles.secondaryBtnTxt}>キャンセル</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
+  }
+
+  getCategoryIcon = (categoryName) => {
+    let icon;
+    switch (categoryName) {
+      case 'カクテル':
+        icon = images.cooktail;
+        break;
+      case 'ワイン':
+        icon = images.wine;
+        break;
+      case 'ビール':
+        icon = images.beer;
+        break;
+      case '日本酒':
+        icon = images.sake;
+        break;
+      case '焼酎':
+        icon = images.syotyu;
+        break;
+      case 'ウイスキー':
+        icon = images.whisky;
+        break;
+      default:
+        icon = images.cooktail;
+        break;
+    }
+    return icon;
   }
 }
--- src/screens/add/styles.js
+++ src/screens/add/styles.js
@@ -5,9 +5,84 @@ import styleConstants from '../../styleConstants';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+  contents: {
+    padding: 16,
+    justifyContent: 'center',
+  },
+  sake: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 32,
+    marginBottom: 32,
+  },
+  icon: {
+    width: 36,
+    height: 36,
+    marginRight: 16,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  starContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 32,
+  },
+  title: {
+    fontSize: 30,
+    height: 50,
+  },
+  textInputContainer: {
+    height: 200,
+    marginBottom: 32,
+    // backgroundColor: 'blue'
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 16,
+    borderColor: 'gray',
+    borderWidth: 0.25,
+    borderRadius: 4,
+    padding: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
+  },
+  btnContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  primaryBtn: {
+    marginVertical: 8,
+    width: 280,
+    height: 36,
+    borderRadius: 18,
+    backgroundColor: 'black',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  },
+  seondaryBtn: {
+    marginVertical: 8,
+    width: 280,
+    height: 36,
+    borderRadius: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  primaryBtnTxt: {
+    fontSize: 16,
+    color: 'white', 
+    fontWeight: '600',
+  },
+  seondaryBtnTxt: {
+    fontSize: 16,
+    color: 'black', 
+    fontWeight: '600',
   },
 });
 
